2c3cfdb8461811445b972b4944742d8ee2401a8b
moollaza <moollaza@fastmail.fm>
2022-12-31 14:32:11 -0500

    More cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,51 +3,18 @@
 	import Header from './Header.svelte';
 </script>
 
-<div class="app">
+<div class="flex flex-col min-height-screen">
 	<Header />
 
-	<main>
+	<main class="flex-1 flex flex-col px-3 py-4 w-100 max-w-5xl border-box py-0 px-auto">
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<footer class="flex flex-col justify-center items-center p-3">
+		<p>RepoRemover by Zaahir Moolla &middot; Copyright &copy; 2023</p>
 	</footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
