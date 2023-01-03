4f3c13f1d8a3f4eb1f89ea8ad862002da399fe42
Zaahir Moolla <moollaza@fastmail.fm>
2023-01-03 04:37:57 +0000

    Format

--- src/lib/state.js
+++ src/lib/state.js
@@ -1,9 +1,6 @@
-import { writable } from "svelte/store";
-
-import { constants } from "$lib/constants";
+import { writable } from 'svelte/store';
+import { constants } from '$lib/constants';
 
 export const accessToken = writable(constants.devToken);
-
 export const ghViewer = writable();
-
 export const ghRepos = writable();
