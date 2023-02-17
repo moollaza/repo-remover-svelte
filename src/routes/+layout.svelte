f2d027c5e038de405096f18b18b2037e7214e434
Zaahir Moolla <moollaza@fastmail.fm>
2023-02-17 05:56:15 +0000

    Create Footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Nav from '$lib/Nav.svelte';
+	import Footer from '$lib/Footer.svelte';
 </script>
 
 <div class="">
@@ -10,17 +11,5 @@
 		<slot />
 	</main>
 
-	<footer class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<div class="flex flex-col border-t border-slate-400/10 py-10 ">
-			<p class="text-sm text-slate-500">
-				Copyright &copy; 2023 <a
-					class="underline underline-offset-4 hover:text-blue-600"
-					target="_blank"
-					rel="noreferrer"
-					href="https://zaahir.ca">Zaahir Moolla</a
-				>
-			</p>
-		</div>
-	</footer>
+	<Footer />
 </div>
-
